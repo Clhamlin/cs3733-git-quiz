c358b40675421d0505947c574a534bf097948677
Gonzo <csgonzo@wpi.edu>
2014-04-04 07:56:02 -0400

    Set to DEMOMODE

--- src/io/brez/application/Application.java
+++ src/io/brez/application/Application.java
@@ -1,32 +1,32 @@
 package io.brez.application;
 
 public class Application {
-	boolean DEMO_MODE = false;
+	boolean DEMO_MODE = true;
 	boolean BROKEN = false;
-	
+
 	public String name() {
 		return "My App";
 	}
-	
+
 	public boolean isWorking() {
-		return isDemoMode() || !isBroken();
+		return isDemoMode() && isBroken() == false;
 	}
-	
+
 	private boolean isBroken() {
 		return BROKEN;
 	}
-	
+
 	private boolean isDemoMode() {
 		return DEMO_MODE;
 	}
-	
+
 	public String text() {
 		return isBroken() ? "BROKEN" : "Totally Fine!";
 	}
-	
+
 	public static void main(String [] args) {
 		Application app = new Application();
-		
+
 		System.out.println("This application is " + app.text() );
 	}
 }
